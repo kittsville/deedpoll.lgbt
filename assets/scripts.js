cb02bf3caecd788d37cef4dcbdd77772571f1970
Kit <kittsville@googlemail.com>
2019-12-01 15:59:42 +0000

    Improve PDF formatting
    - Use standard 25.4mm margins
    - Add signature sections
    - Use Gendered Intelligence wording
    
    #3

--- assets/scripts.js
+++ assets/scripts.js
@@ -10,25 +10,31 @@ getDateSuffix = day => {
   }
 }
 
-getPrettyDate = (e, fieldName) => {
+getDeedPollText = e => {
+  const oldName   = getFormValue(e, "old-name"),
+  newName         = getFormValue(e, "new-name"),
+  address         = getFormValue(e, "address"),
+  witness1Name    = getFormValue(e, "witness-1-name"),
+  witness1Address = getFormValue(e, "witness-1-address"),
+  witness2Name    = getFormValue(e, "witness-2-name"),
+  witness2Address = getFormValue(e, "witness-2-address");
+
   const months = ["January", "February", "March", "April", "May", "June", "July",
   "August", "September", "October", "November", "December"];
-  const date = new Date(getFormValue(e, fieldName));
-
-  const day = date.getDate(),
+  date = new Date(getFormValue(e, "date")),
+  day = date.getDate(),
   month = months[date.getMonth()],
   year = date.getFullYear();
 
-  return `${day}${getDateSuffix(day)} ${month} ${year}`;
+  return [
+    `By this deed of change of name made by myself the undersigned ${newName} of ${address} formerly known as ${oldName}.`,
+    'Hereby declare as follows:',
+    `I absolutely and entirely renounce, relinquish and abandon the use of my former name of ${oldName} and assume, adopt and determine to take and use from the date hereof the name of ${newName} in substitution for my former name of ${oldName}.`,
+    `I shall at all times hereafter in all records, deeds, documents and other writings and in all actions and proceedings, as well as in all dealings and transactions and on all occasions whatsoever use and subscribe the said name of ${newName} as my name, in substitution for my former name of ${oldName} so relinquished as aforesaid to the intent that I may hereafter be called, known or distinguished by the name of ${newName} only and not by my former name of ${oldName}.`,
+    `I authorise and require all persons at all times to designate, describe and address me by the adopted name of ${newName}.`,
+    `Dated this ${day}${getDateSuffix(day)} day of ${month} in the year ${year}.`
+  ];
 }
-
-getDeedPollText = e => `I ${getFormValue(e, "old-name")} of ${getFormValue(e, "address")} have given up my name ${getFormValue(e, "old-name")} and have adopted for all purposes the name ${getFormValue(e, "new-name")}.
-
-Signed as a deed on ${getPrettyDate(e, "date")} as ${getFormValue(e, "old-name")} and ${getFormValue(e, "new-name")} in the presence of ${getFormValue(e, "witness-1-name")} of ${getFormValue(e, "witness-1-address")}, and ${getFormValue(e, "witness-2-name")} of ${getFormValue(e, "witness-2-address")}.
-
-[your new signature], [your old signature]
-
-[witness 1 signature], [witness 2 signature]`;
 
 getCurrentDate = () => {
   const date = new Date(),
@@ -38,6 +44,17 @@ getCurrentDate = () => {
 
   return `${year}-${month}-${day}`;
 };
+
+drawSignatureBox = (doc, xStart, y, text) => {
+  const lineLength = 55;
+
+  const xEnd = xStart + lineLength;
+  doc.line(xStart, y, xEnd, y);
+
+  const textXStart = xStart + (lineLength / 2),
+  textYStart = y + 6;
+  doc.text(text, textXStart, textYStart, 'center');
+}
 
 document.getElementById('date').value = getCurrentDate();
 
@@ -63,13 +80,21 @@ document.getElementById('user-info').addEventListener('submit', e => {
   );
 
   doc.setFont('LinLibertine_R');
-  doc.setFontSize(14);
+  doc.setFontSize(12);
 
   doc.text(
-    getDeedPollText(e), 10, 55, {
-      maxWidth: '180'
+    getDeedPollText(e), 25.4, 55, {
+      maxWidth: '159.2'
     }
   );
+
+  doc.setLineWidth(0.4);
+  doc.setFontSize(10);
+  drawSignatureBox(doc, 35, 200, ['by the above name', getFormValue(e, "new-name")]);
+  drawSignatureBox(doc, 120, 200, ['by the above name', getFormValue(e, "old-name")]);
+
+  drawSignatureBox(doc, 35, 240, [getFormValue(e, "witness-1-name"), getFormValue(e, "witness-1-address")]);
+  drawSignatureBox(doc, 120, 240, [getFormValue(e, "witness-2-name"), getFormValue(e, "witness-2-address")]);
 
   doc.save('a4.pdf');
 });
