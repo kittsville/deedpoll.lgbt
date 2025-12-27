2ce42860eeff8378bad146ee1583e82abb21af31
Kit <kittsville@googlemail.com>
2025-12-27 17:10:53 +0000

    Fix date input parsing

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,23 @@
+const months = [
+  ["January", "Jan"],
+  ["February", "Feb"],
+  ["March", "Mar"],
+  ["April", "Apr"],
+  ["May", "May"],
+  ["June", "Jun"],
+  ["July", "Jul"],
+  ["August", "Aug"],
+  ["September", "Sep", "Sept"],
+  ["October", "Oct"],
+  ["November", "Nov"],
+  ["December", "Dec"]
+];
+
+const getMonth = month => {
+  if (/^\d+$/.test(month)) return months[month - 1][0];
+  return months.find(monthNames => monthNames.find(name => name.toLowerCase() === month.toLowerCase()))[0];
+};
+
 const getFormValue = (e, fieldName) => e.srcElement.elements[fieldName].value;
 
 const getDateSuffix = day => {
@@ -16,7 +36,7 @@ const getCurrentDate = () => {
   day = date.getDate().toString().padStart(2, '0'),
   year = date.getFullYear();
 
-  return `${year}-${month}-${day}`;
+  return [year, month, day];
 };
 
 const showForm = () => location.hash = '';
@@ -24,9 +44,11 @@ const showDeedPoll = () => location.hash = 'deedpoll';
 const onHashChange = () => {
   if (location.hash === '#deedpoll') {
     document.getElementById('data-entry').hidden = true;
+    document.getElementById('footer').hidden = true;
     document.getElementById('printable-deed-poll').hidden = false;
   } else {
     document.getElementById('data-entry').hidden = false;
+    document.getElementById('footer').hidden = false;
     document.getElementById('printable-deed-poll').hidden = true;
   }
   scroll(0,0);
@@ -53,20 +75,21 @@ const generateDeedPoll = e => {
     templateDeedPoll(key, value);
   });
 
-  const date = new Date(getFormValue(e, "date")),
-  months = ["January", "February", "March", "April", "May", "June", "July",
-  "August", "September", "October", "November", "December"],
-  day = date.getDate();
+  const day = getFormValue(e, "deed-poll-signed-day"), rawMonth = getFormValue(e, "deed-poll-signed-month"), year = getFormValue(e, "deed-poll-signed-year");
+  const month = getMonth(rawMonth);
 
   templateDeedPoll('day', day);
   templateDeedPoll('day-suffix', getDateSuffix(day));
-  templateDeedPoll('month', months[date.getMonth()]);
-  templateDeedPoll('year', date.getFullYear());
+  templateDeedPoll('month', month);
+  templateDeedPoll('year', year);
 
   showDeedPoll();
 }
 
-document.getElementById('date').value = getCurrentDate();
+const [currentYear, currentMonth, currentDay] = getCurrentDate();
+document.getElementById('deed-poll-signed-year').value = currentYear;
+document.getElementById('deed-poll-signed-month').value = currentMonth;
+document.getElementById('deed-poll-signed-day').value = currentDay;
 document.getElementById('user-info').addEventListener('submit', generateDeedPoll);
 document.getElementById('back').addEventListener('click', showForm);
 document.getElementById('see-example').addEventListener('click', showDeedPoll);
